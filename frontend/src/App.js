5cc6593f4c110383e40ba89e8d6dcee79b542d22
Daniel Roithmeier <roithmeierda@gmail.com>
2023-10-17 03:25:33 +0200

    revised Aopp.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,7 @@
 import "./App.css"
 
-import Header from "./components/Header.js"
-import Footer from "./components/Footer.js"
 import Home from "./pages/Home.js"
 
 export default function App() {
-  return (
-    <>
-      <Header headline="home" />
-      <main>
-        <Home />
-      </main>
-      <Footer />
-    </>
-  )
+  return <Home />
 }
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,15 +3,24 @@ import "./Home.css"
 import Recipes from "../assets/images/recipes.png"
 import Ingredients from "../assets/images/ingredients.png"
 
+import Header from "../components/Header.js"
+import Footer from "../components/Footer.js"
+
 export default function Home() {
   return (
-    <section className="main-content">
-      <a href="#" className="recipes-anchor">
-        <img src={Recipes} alt="Rezepte" />
-      </a>
-      <a href="#" className="ingredients-anchor">
-        <img src={Ingredients} alt="Zutaten" />
-      </a>
-    </section>
+    <>
+      <Header headline="home" />
+      <main>
+        <section className="main-content">
+          <a href="#" className="recipes-anchor">
+            <img src={Recipes} alt="Rezepte" />
+          </a>
+          <a href="#" className="ingredients-anchor">
+            <img src={Ingredients} alt="Zutaten" />
+          </a>
+        </section>
+      </main>
+      <Footer />
+    </>
   )
 }
