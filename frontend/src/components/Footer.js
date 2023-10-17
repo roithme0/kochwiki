198d599925033c6d103954ad513d6f07301e9aac
Daniel Roithmeier <roithmeierda@gmail.com>
2023-10-17 02:58:19 +0200

    added images to header & footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,11 +1,13 @@
 import "./Footer.css"
 
+import AccountStarOutline from "../assets/images/mdi/account-star-outline.png"
+
 export default function Footer() {
   return (
     <footer>
       <section className="footer-content">
         <a href="#" className="admin-anchor">
-          <img src="#" alt="Admin" className="admin" />
+          <img src={AccountStarOutline} alt="Admin" className="admin" />
         </a>
       </section>
     </footer>
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,15 +1,18 @@
 import "./Header.css"
 
+import Logo from "../assets/images/logo.png"
+import ArrowLeft from "../assets/images/mdi/arrow-left.png"
+
 export default function Header({ headline }) {
   return (
     <header>
       <section className="header-content">
         <a href="#" className="logo-anchor">
-          <img src="#" alt="Logo" className="logo" />
+          <img src={Logo} alt="Logo" className="logo" />
         </a>
         <h1>{headline}</h1>
         <a href="#" className="back-anchor">
-          <img src="#" alt="Zurück" className="back" />
+          <img src={ArrowLeft} alt="Zurück" className="back" />
         </a>
       </section>
     </header>
