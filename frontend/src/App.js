4f07efedff99b9f2cce564918624e9c18d5240b6
Daniel Roithmeier <roithmeierda@gmail.com>
2023-10-17 02:35:09 +0200

    added footer plus minimal formatting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,14 @@
 import "./App.css"
 
 import Header from "./components/Header.js"
+import Footer from "./components/Footer.js"
 
 export default function App() {
-  return <Header headline="home" />
+  return (
+    <>
+      <Header headline="home" />
+      <div className="content"></div>
+      <Footer />
+    </>
+  )
 }
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,13 @@
+import "./Footer.css"
+
+export default function Footer() {
+  return (
+    <footer>
+      <section className="footer-content">
+        <a href="#" className="admin-anchor">
+          <img src="#" alt="Admin" className="admin" />
+        </a>
+      </section>
+    </footer>
+  )
+}
