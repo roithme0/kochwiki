56deefbb3801d7b49eb050bc42d67a0ae85a4207
Daniel Roithmeier <roithmeierda@gmail.com>
2023-11-06 11:33:25 +0100

    revised popup

--- frontend/src/components/Popup.js
+++ frontend/src/components/Popup.js
@@ -3,7 +3,6 @@ import "./Popup.css"
 export default function Popup({ Component, ...props }) {
   return (
     <div className="popup">
-      <div className="background"></div>
       <div className="popup-wrapper">
         <Component {...props}></Component>
       </div>
