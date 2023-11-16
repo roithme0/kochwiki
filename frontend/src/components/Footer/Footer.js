2c2d778f7970304ec974d2cd71bfa3c86066dd9e
Daniel Roithmeier <roithmeierda@gmail.com>
2023-11-16 17:04:39 +0100

    modular css for header and footer

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,17 +1,16 @@
-import "./Footer.css"
+import css from "./Footer.module.css"
 
 import accountStarOutline from "../../assets/images/mdi/account-star-outline.png"
-
 import { Link } from "react-router-dom"
 
 export default function Footer() {
   // render footer
 
   return (
-    <footer>
-      <section className="footer-content">
-        <Link to="/admin" className="admin-anchor">
-          <img src={accountStarOutline} alt="Admin" className="admin" />
+    <footer className={css.footer}>
+      <section className={css.footerContent}>
+        <Link to="/admin">
+          <img src={accountStarOutline} alt="Admin" />
         </Link>
       </section>
     </footer>
--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import "./Header.css"
+import css from "./Header.module.css"
 
 import logo from "../../assets/images/logo.png"
 import arrowLeft from "../../assets/images/mdi/arrow-left.png"
@@ -9,14 +9,14 @@ export default function Header({ headline, back }) {
   // render header
 
   return (
-    <header>
-      <section className="header-content">
-        <Link to="/" className="logo-anchor">
-          <img src={logo} alt="Logo" className="logo" />
+    <header className={css.header}>
+      <section className={css.headerContent}>
+        <Link to="/">
+          <img src={logo} alt="Logo" />
         </Link>
-        <h1>{headline}</h1>
-        <Link to={back.url} className={"back-anchor " + back.visibility}>
-          <img src={arrowLeft} alt="Zurück" className="back" />
+        <h1 className={css.h1}>{headline}</h1>
+        <Link to={back.url} className={css[back.visibility]}>
+          <img src={arrowLeft} alt="Zurück" />
         </Link>
       </section>
     </header>
