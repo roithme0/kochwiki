389b6b21a2e4c50b55360e9fb14f63a39c594c50
Daniel Roithmeier <roithmeierda@gmail.com>
2023-11-17 14:44:02 +0100

    sort columns to render

--- frontend/src/components/IngredientsGrid/IngredientsGrid.js
+++ frontend/src/components/IngredientsGrid/IngredientsGrid.js
@@ -9,17 +9,46 @@ import { useEffect, useState } from "react"
 export default function IngredientsGrid({ ingredients }) {
   // render header and grid of ingredients
 
+  const columnsOrder = [
+    "name",
+    "brand",
+    "kcal",
+    "carbs",
+    "protein",
+    "fat",
+    "unit",
+    "edit",
+    "delete",
+  ]
+
   const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+  const [columnsToRender, setColumnsToRender] = useState([
+    "name",
+    "brand",
+    "edit",
+    "delete",
+  ])
   const [search, setSearch] = useState("")
   const [filter, setFilter] = useState("")
   const [sortKey, setSortKey] = useState("")
 
   useEffect(() => {
+    // track window width
     const handleResize = () => setWindowWidth(window.innerWidth)
     window.addEventListener("resize", handleResize)
     console.debug("windowWidth: ", windowWidth)
     return () => window.removeEventListener("resize", handleResize)
   })
+
+  useEffect(() => {
+    // update columns to render when window width changes
+    var columns = getColumnsToRender({ windowWidth: windowWidth })
+    columns = sortColumnsToRender({
+      columnsToRender: columns,
+      columnsOrder: columnsOrder,
+    })
+    setColumnsToRender(columns)
+  }, [windowWidth])
 
   return (
     <div className={css.ingredientsGrid}>
@@ -31,22 +60,39 @@ export default function IngredientsGrid({ ingredients }) {
         verboseNames={ingredients[0].verbose_names}
         sortKey={sortKey}
         setSortKey={setSortKey}
+        columnsToRender={columnsToRender}
+        columnsOrder={columnsOrder}
       />
       <IngredientsGridBody
         initialIngredients={ingredients}
         search={search}
         filter={filter}
         sortKey={sortKey}
+        columnsToRender={columnsToRender}
+        columnsOrder={columnsOrder}
       />
     </div>
   )
 }
 
-function getColumnsToRender(windowWidth) {
+function getColumnsToRender({ windowWidth }) {
   // return the columns to render based on window width
   var columns = ["name", "brand", "edit", "delete"]
   windowWidth > 600 && columns.push("kcal")
   windowWidth > 700 && columns.push("unit")
   windowWidth > 1200 && columns.push("carbs", "protein", "fat")
+  console.debug("columnsToRender: ", columns)
   return columns
+}
+
+function sortColumnsToRender({ columnsToRender, columnsOrder }) {
+  // sort columns to render based on columns order
+  var sortedColumns = []
+  columnsOrder.forEach(column => {
+    if (columnsToRender.includes(column)) {
+      sortedColumns.push(column)
+    }
+  })
+  console.debug("sortedColumns: ", sortedColumns)
+  return sortedColumns
 }
