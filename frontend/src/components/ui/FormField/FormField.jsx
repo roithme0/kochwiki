856cd8d09f41c25b8d74fd71273b07ff401de1e5
Daniel Roithmeier <roithmeierda@gmail.com>
2023-11-20 20:41:12 +0100

    improve feeling of search

--- frontend/src/components/ui/FormField/FormField.jsx
+++ frontend/src/components/ui/FormField/FormField.jsx
@@ -6,10 +6,9 @@ export default function FormField({ label, type, classNameInput }) {
   return (
     <>
       <label className={css.label}>{label}</label>
-      {type === "selectUnit" && (
+      {type === "selectUnit" ? (
         <IngredientUnitSelect classNameSelect={classNameInput} />
-      )}
-      {type !== "selectUnit" && (
+      ) : (
         <input type={type} className={classNameInput} />
       )}
     </>
--- frontend/src/components/ui/Search/Search.jsx
+++ frontend/src/components/ui/Search/Search.jsx
@@ -1,24 +1,56 @@
 import css from "./Search.module.css"
 
-import { mdiMagnify } from "@mdi/js"
+import { mdiMagnify, mdiClose } from "@mdi/js"
 import Button from "../Button"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 export default function Search({ setSearch }) {
+  const [searchLocal, setSearchLocal] = useState("")
+
   useEffect(() => {
-    // set initial search
-    setSearch("")
-  }, [])
+    // update search + look of search field
+    setSearch(searchLocal)
+    if (searchLocal.length > 0) {
+      getSearchElement().classList.add(css.active)
+    } else {
+      getSearchElement().classList.remove(css.active)
+    }
+  }, [searchLocal])
 
   return (
-    <div className={css.search}>
+    <div className={css.search} id={css.search}>
       <input
         type="text"
+        value={searchLocal}
         placeholder="Name oder Marke suchen ..."
         className={css.searchField}
-        onChange={event => setSearch(event.target.value)}
+        id={css.searchField}
+        onChange={event => setSearchLocal(event.target.value)}
       />
-      <Button svg={mdiMagnify} className={css.magnify} />
+      {searchLocal.length ? (
+        <Button
+          svg={mdiClose}
+          className={css.clear}
+          clickHandler={() => {
+            setSearchLocal("")
+            getSearchFieldElement().focus()
+          }}
+        />
+      ) : (
+        <Button
+          svg={mdiMagnify}
+          className={css.magnify}
+          clickHandler={() => getSearchFieldElement().focus()}
+        />
+      )}
     </div>
   )
+}
+
+function getSearchElement() {
+  return document.getElementById(`${css.search}`)
+}
+
+function getSearchFieldElement() {
+  return document.getElementById(`${css.searchField}`)
 }
