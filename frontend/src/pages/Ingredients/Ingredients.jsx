548d5e11aaefdfa5ff15f9f1eeb4a5bf0366d23e
Daniel Roithmeier <roithmeierda@gmail.com>
2023-11-21 23:34:12 +0100

    fixed bug when creating first ingredient

--- frontend/src/pages/Ingredients/Ingredients.jsx
+++ frontend/src/pages/Ingredients/Ingredients.jsx
@@ -3,6 +3,7 @@ import css from "./Ingredients.module.css"
 import { mdiPlus } from "@mdi/js"
 import { useState, useEffect } from "react"
 import getIngredients from "../../services/api/Ingredient/getIngredients"
+import getIngredient from "../../services/api/Ingredient/getIngredient"
 import IngredientsGrid from "../../components/Ingredients/IngredientsGrid"
 import Popup from "../../components/popups/Popup"
 import IngredientAddPopup from "../../components/popups/IngredientAddPopup"
@@ -80,16 +81,27 @@ export default function Ingredients({ setHeadline, setBack, setButtons }) {
   )
 }
 
-function updateIngredient({ changedIngredient, ingredients, setIngredients }) {
+async function updateIngredient({
+  changedIngredient,
+  ingredients,
+  setIngredients,
+}) {
   // update ingredient in ingredients array or add it if it doesn't exist yet
   if (changedIngredient === null) {
     console.debug("no changes were made")
     return
   }
+
+  const result = await getIngredient({ id: changedIngredient.id })
+  if (result.success === false) {
+    console.error("could not update or add ingredient: ", changedIngredient.id)
+    return
+  }
+
   console.debug("updating or adding ingredient: ", changedIngredient.id)
   var updatedIngredients = ingredients.filter(
     ingredient => ingredient.id !== changedIngredient.id
   )
-  updatedIngredients = [...updatedIngredients, changedIngredient]
+  updatedIngredients.push(result.fetchedIngredient)
   setIngredients(updatedIngredients)
 }
