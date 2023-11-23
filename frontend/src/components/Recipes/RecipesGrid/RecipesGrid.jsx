2721dc4ff723eb8c875b89098f679c09337743d6
Daniel Roithmeier <roithmeierda@gmail.com>
2023-11-23 17:10:10 +0100

    progress on recipes grid

--- frontend/src/components/Recipes/RecipesGrid/RecipesGrid.jsx
+++ frontend/src/components/Recipes/RecipesGrid/RecipesGrid.jsx
@@ -4,16 +4,13 @@ import RecipesGridElement from "../RecipesGridElement"
 
 export default function RecipesGrid({ recipes }) {
   return (
-    <>
-      <div className={css.controls}>
-        <p>Search</p>
-        <p>Filter</p>
-      </div>
+    <div className={css.gridWrapper}>
+      <div className={css.controls}>Controls</div>
       <div className={css.recipesWrapper}>
         {recipes.map(recipe => (
           <RecipesGridElement recipe={recipe} />
         ))}
       </div>
-    </>
+    </div>
   )
 }
--- frontend/src/components/Recipes/RecipesGridElement/RecipesGridElement.jsx
+++ frontend/src/components/Recipes/RecipesGridElement/RecipesGridElement.jsx
@@ -1,5 +1,13 @@
 import css from "./RecipesGridElement.module.css"
 
+import Button from "../../ui/Button"
+import { mdiImageOffOutline } from "@mdi/js"
+
 export default function RecipesGridElement({ recipe }) {
-  return <p>{recipe.name}</p>
+  return (
+    <div className={css.elementWrapper}>
+      <Button svg={mdiImageOffOutline} className={css.image} />
+      <p>{recipe.name}</p>
+    </div>
+  )
 }
