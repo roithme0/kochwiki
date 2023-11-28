5d31207eb1da31325dd1b2e769dc4515aa2eee2a
Daniel Roithmeier <roithmeierda@gmail.com>
2023-11-28 15:48:13 +0100

    basic home page

--- angular/src/app/home/home.component.ts
+++ angular/src/app/home/home.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
