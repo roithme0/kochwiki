5ded2d70d94959d57edfba60c2f25da375b3d58a
Daniel Roithmeier <roithmeierda@gmail.com>
2023-11-28 16:06:42 +0100

    sketched out header and footer

--- angular/src/app/page-header/page-header.component.ts
+++ angular/src/app/page-header/page-header.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-page-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, RouterLink, RouterLinkActive],
   templateUrl: './page-header.component.html',
-  styleUrl: './page-header.component.css'
+  styleUrl: './page-header.component.css',
 })
-export class PageHeaderComponent {
-
-}
+export class PageHeaderComponent {}
