7c2ffe3d3c37cd81c73cc7610939c746989c3628
Daniel Roithmeier <roithmeierda@gmail.com>
2023-11-28 12:03:21 +0100

    prepared views

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { IngredientsListComponent } from './ingredients-list/ingredients-list.component';
+import { RecipesListComponent } from './recipes-list/recipes-list.component';
+import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent, title: 'Home' },
+  {
+    path: 'ingredients',
+    component: IngredientsListComponent,
+    title: 'Zutaten',
+  },
+  { path: 'recipes', component: RecipesListComponent, title: 'Rezepte' },
+  {
+    path: 'recipes/:id',
+    component: RecipeDetailsComponent,
+    title: 'Rezept Details',
+  },
+];
---
+++ angular/src/app/recipes-list/recipes-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-recipes-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './recipes-list.component.html',
+  styleUrl: './recipes-list.component.css'
+})
+export class RecipesListComponent {
+
+}
