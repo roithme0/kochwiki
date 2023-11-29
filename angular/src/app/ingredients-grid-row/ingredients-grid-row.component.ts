22afee9b44a20f3c7edbd2de2e19b63e59a81f71
Daniel Roithmeier <roithmeierda@gmail.com>
2023-11-29 14:20:27 +0100

    finished grid sketch

--- angular/src/app/ingredients-grid-row/ingredients-grid-row.component.ts
+++ angular/src/app/ingredients-grid-row/ingredients-grid-row.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Ingredient } from '../interfaces/ingredient';
 
 @Component({
   selector: 'app-ingredients-grid-row',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './ingredients-grid-row.component.html',
-  styleUrl: './ingredients-grid-row.component.css'
+  styleUrl: './ingredients-grid-row.component.css',
 })
 export class IngredientsGridRowComponent {
-
+  @Input() ingredient!: Ingredient;
 }
