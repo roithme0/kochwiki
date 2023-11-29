3a7436dd8e4292e96b8f3e2bf3f24ed764f27faa
Daniel Roithmeier <roithmeierda@gmail.com>
2023-11-29 19:38:27 +0100

    added basic form

--- angular/src/app/dialogs/edit-ingredient-dialog/edit-ingredient-dialog.component.ts
+++ angular/src/app/dialogs/edit-ingredient-dialog/edit-ingredient-dialog.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-ingredient-dialog',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './edit-ingredient-dialog.component.html',
-  styleUrl: './edit-ingredient-dialog.component.css'
+  styleUrl: './edit-ingredient-dialog.component.css',
 })
 export class EditIngredientDialogComponent {
+  units = ['g', 'ml', 'Stk.'];
 
+  ingredientForm = new FormGroup({
+    name: new FormControl(''),
+    brand: new FormControl(''),
+    unit: new FormControl(''),
+    kcal: new FormControl(''),
+    makros: new FormGroup({
+      carbs: new FormControl(''),
+      protein: new FormControl(''),
+      fat: new FormControl(''),
+    }),
+  });
+
+  onSubmit() {
+    console.log(this.ingredientForm.value);
+  }
 }
