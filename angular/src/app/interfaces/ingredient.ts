a227d606a10df1d45f29d1d96b5a099c602f717a
Daniel Roithmeier <roithmeierda@gmail.com>
2023-11-29 12:44:48 +0100

    prepared interfaces

---
+++ angular/src/app/interfaces/ingredient.ts
@@ -0,0 +1,10 @@
+export interface Ingredient {
+  id: number;
+  name: string;
+  brand: string;
+  unit: string;
+  kcal: number;
+  carbs: number;
+  protein: number;
+  fat: number;
+}
---
+++ angular/src/app/interfaces/recipe.ts
@@ -0,0 +1,10 @@
+export interface Recipe {
+  id: number;
+  name: string;
+  servings: number;
+  preptime: number;
+  originName: string;
+  originUrl: string;
+  original: string;
+  image: string;
+}
