e4972ccb73934208a9e5be4d45f7368197592b6b
Daniel Roithmeier <roithmeierda@gmail.com>
2023-12-01 13:24:46 +0100

    moved fetching ingredients to grid

--- angular/src/app/ingredients-grid/ingredients-grid.component.ts
+++ angular/src/app/ingredients-grid/ingredients-grid.component.ts
@@ -1,8 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Ingredient } from '../interfaces/ingredient';
+import { IngredientService } from '../services/ingredient/ingredient.service';
 import { IngredientsGridHeaderComponent } from '../ingredients-grid-header/ingredients-grid-header.component';
 import { IngredientsGridRowComponent } from '../ingredients-grid-row/ingredients-grid-row.component';
+import { MatDialog } from '@angular/material/dialog';
+import { CreateIngredientDialogComponent } from '../dialogs/create-ingredient-dialog/create-ingredient-dialog.component';
 
 @Component({
   selector: 'app-ingredients-grid',
@@ -16,5 +19,30 @@ import { IngredientsGridRowComponent } from '../ingredients-grid-row/ingredients
   styleUrl: './ingredients-grid.component.css',
 })
 export class IngredientsGridComponent {
-  @Input() ingredients!: Ingredient[];
+  dialog: MatDialog = inject(MatDialog);
+  ingredientService: IngredientService = inject(IngredientService);
+  ingredients: Ingredient[] = [];
+
+  constructor() {
+    this.fetchIngredients();
+    this.ingredientService.ingredients$.subscribe(() => {
+      this.fetchIngredients();
+    });
+  }
+
+  fetchIngredients(): void {
+    this.ingredientService.getAllIngredients().subscribe({
+      next: (ingredients) => {
+        console.debug('ingredients fetched: ', ingredients);
+        this.ingredients = ingredients;
+      },
+      error: (error) => {
+        console.error('failed to fetch ingredients: ', error);
+      },
+    });
+  }
+
+  openCreateIngredientDialog(): void {
+    this.dialog.open(CreateIngredientDialogComponent);
+  }
 }
--- angular/src/app/ingredients/ingredients.component.ts
+++ angular/src/app/ingredients/ingredients.component.ts
@@ -2,11 +2,7 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IngredientsGridComponent } from '../ingredients-grid/ingredients-grid.component';
 import { PageHeaderService } from '../services/page-header/page-header.service';
-import { PageFooterService } from '../services/page-footer/page-footer.service';
-import { IngredientService } from '../services/ingredient/ingredient.service';
-import { Ingredient } from '../interfaces/ingredient';
 import { MatDialog } from '@angular/material/dialog';
-import { CreateIngredientDialogComponent } from '../dialogs/create-ingredient-dialog/create-ingredient-dialog.component';
 
 @Component({
   selector: 'app-ingredients',
@@ -18,41 +14,9 @@ import { CreateIngredientDialogComponent } from '../dialogs/create-ingredient-di
 export class IngredientsComponent {
   dialog: MatDialog = inject(MatDialog);
   pageHeaderService: PageHeaderService = inject(PageHeaderService);
-  pageFooterService: PageFooterService = inject(PageFooterService);
-  ingredientService: IngredientService = inject(IngredientService);
-  ingredients: Ingredient[] = [];
-
-  constructor() {
-    this.fetchIngredients();
-    this.ingredientService.ingredients$.subscribe(() => {
-      this.fetchIngredients();
-    });
-  }
 
   ngOnInit() {
     this.pageHeaderService.setHeadline('Zutaten');
     this.pageHeaderService.setBack('');
-    this.pageFooterService.setButtons([
-      {
-        text: 'Zutat hinzufügen',
-        action: () => this.openCreateIngredientDialog(),
-      },
-    ]);
-  }
-
-  fetchIngredients(): void {
-    this.ingredientService.getAllIngredients().subscribe({
-      next: (ingredients) => {
-        console.debug('ingredients fetched: ', ingredients);
-        this.ingredients = ingredients;
-      },
-      error: (error) => {
-        console.error('failed to fetch ingredients: ', error);
-      },
-    });
-  }
-
-  openCreateIngredientDialog(): void {
-    this.dialog.open(CreateIngredientDialogComponent);
   }
 }
