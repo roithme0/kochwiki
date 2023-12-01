96fc3f1578d63ba636ca9bc01e089afe6d009427
Daniel Roithmeier <roithmeierda@gmail.com>
2023-12-01 17:42:19 +0100

    utilize signals

--- angular/src/app/ingredients-grid/ingredients-grid.component.ts
+++ angular/src/app/ingredients-grid/ingredients-grid.component.ts
@@ -1,4 +1,11 @@
-import { Component, inject } from '@angular/core';
+import {
+  Component,
+  Signal,
+  WritableSignal,
+  computed,
+  inject,
+  signal,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Ingredient } from '../interfaces/ingredient';
 import { IngredientService } from '../services/ingredient/ingredient.service';
@@ -27,19 +34,22 @@ export class IngredientsGridComponent {
   ingredientsGridControlsService: IngredientsGridControlsService = inject(
     IngredientsGridControlsService
   );
-  searchBy!: string;
-  filterBy!: string;
-  ingredients: Ingredient[] = [];
+  ingredients: WritableSignal<Ingredient[]> = signal([]);
+  displayedIngredients: Signal<Ingredient[]> = computed(() =>
+    this.getDisplayedIngredients()
+  );
+  searchBy: WritableSignal<string> = signal('');
+  filterBy: WritableSignal<string> = signal('all');
 
   constructor() {
     this.ingredientService.ingredients$.subscribe(() => {
       this.fetchIngredients();
     });
     this.ingredientsGridControlsService.searchBy$.subscribe((searchBy) => {
-      this.searchBy = searchBy;
+      this.searchBy.set(searchBy);
     });
     this.ingredientsGridControlsService.filterBy$.subscribe((filterBy) => {
-      this.filterBy = filterBy;
+      this.filterBy.set(filterBy);
     });
   }
 
@@ -51,7 +61,7 @@ export class IngredientsGridComponent {
     this.ingredientService.getAllIngredients().subscribe({
       next: (ingredients) => {
         console.debug('ingredients fetched: ', ingredients);
-        this.ingredients = ingredients;
+        this.ingredients.set(ingredients);
       },
       error: (error) => {
         console.error('failed to fetch ingredients: ', error);
@@ -61,5 +71,45 @@ export class IngredientsGridComponent {
 
   openCreateIngredientDialog(): void {
     this.dialog.open(CreateIngredientDialogComponent);
+  }
+
+  getDisplayedIngredients(): Ingredient[] {
+    var displayedIngredients = this.ingredients();
+    displayedIngredients = this.searchIngredientsByNameOrBrand(
+      displayedIngredients,
+      this.searchBy()
+    );
+    displayedIngredients = this.filterIngredientsByUnit(
+      displayedIngredients,
+      this.filterBy()
+    );
+    return displayedIngredients;
+  }
+
+  searchIngredientsByNameOrBrand(
+    ingredients: Ingredient[],
+    searchBy: string
+  ): Ingredient[] {
+    if (searchBy === '') {
+      return ingredients;
+    }
+    return ingredients.filter((ingredient) => {
+      return (
+        ingredient.name.toLowerCase().includes(searchBy.toLowerCase()) ||
+        ingredient.brand.toLowerCase().includes(searchBy.toLowerCase())
+      );
+    });
+  }
+
+  filterIngredientsByUnit(
+    ingredients: Ingredient[],
+    filterBy: string
+  ): Ingredient[] {
+    if (filterBy === 'all') {
+      return ingredients;
+    }
+    return ingredients.filter((ingredient) => {
+      return ingredient.unit === filterBy;
+    });
   }
 }
