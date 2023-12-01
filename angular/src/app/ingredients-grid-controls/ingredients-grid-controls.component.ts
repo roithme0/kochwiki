0b8cb072562a75d15e958d7fa28d334530a93a7c
Daniel Roithmeier <roithmeierda@gmail.com>
2023-12-01 16:31:50 +0100

    renamed

--- angular/src/app/ingredients-grid-controls/ingredients-grid-controls.component.ts
+++ angular/src/app/ingredients-grid-controls/ingredients-grid-controls.component.ts
@@ -3,13 +3,13 @@ import { CommonModule } from '@angular/common';
 import { IngredientsGridControlsService } from '../services/ingredients-grid-controls/ingredients-grid-controls.service';
 
 @Component({
-  selector: 'app-ingredients-grid-control',
+  selector: 'app-ingredients-grid-controls',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './ingredients-grid-control.component.html',
-  styleUrl: './ingredients-grid-control.component.css',
+  templateUrl: './ingredients-grid-controls.component.html',
+  styleUrl: './ingredients-grid-controls.component.css',
 })
-export class IngredientsGridControlComponent {
+export class IngredientsGridControlsComponent {
   searchBy: string = '';
   filterBy: string = 'all';
   ingredientsGridControlsService: IngredientsGridControlsService = inject(
