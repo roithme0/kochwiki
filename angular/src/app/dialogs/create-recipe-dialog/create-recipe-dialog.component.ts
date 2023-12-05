6f63e298806b57065c13d258b2bd16ec41c48ae6
Daniel Roithmeier <roithmeierda@gmail.com>
2023-12-05 15:37:42 +0100

    ...

--- angular/src/app/dialogs/create-recipe-dialog/create-recipe-dialog.component.ts
+++ angular/src/app/dialogs/create-recipe-dialog/create-recipe-dialog.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Recipe } from '../../interfaces/recipe';
+import { RecipeService } from '../../services/recipe/recipe.service';
 
 @Component({
   selector: 'app-create-recipe-dialog',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './create-recipe-dialog.component.html',
-  styleUrl: './create-recipe-dialog.component.css'
+  styleUrl: './create-recipe-dialog.component.css',
 })
 export class CreateRecipeDialogComponent {
-
+  constructor(private recipeService: RecipeService) {}
 }
