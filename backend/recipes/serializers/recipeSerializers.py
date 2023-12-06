a0135704374a00f713e9b6298960c878bf6ab6b7
Daniel Roithmeier <roithmeierda@gmail.com>
2023-12-06 20:46:50 +0100

    simplified

--- backend/recipes/serializers/recipeSerializers.py
+++ backend/recipes/serializers/recipeSerializers.py
@@ -5,9 +5,7 @@ from ..serializers.stepSerializers import StepSerializer
 from rest_framework import serializers
 import logging
 
-
 logger = logging.getLogger("django")
-
 
 class AddRecipeSerializer(serializers.ModelSerializer):
     field_errors = serializers.SerializerMethodField()
@@ -36,7 +34,6 @@ class AddRecipeSerializer(serializers.ModelSerializer):
             return obj.non_field_errors
         else:
             return []
-
 
 class EditRecipeSerializer(serializers.ModelSerializer):
     field_errors = serializers.SerializerMethodField()
@@ -67,7 +64,6 @@ class EditRecipeSerializer(serializers.ModelSerializer):
         else:
             return []
 
-
 class RecipeSerializer(serializers.ModelSerializer):
     amounts = AmountSerializer(many=True)
     steps = StepSerializer(many=True)
@@ -80,68 +76,52 @@ class RecipeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipe
         fields = "__all__"
+     
+    def get_choices(self, obj):
+        choices = {}
+        for field in self.Meta.model._meta.get_fields():
+            if not field.get_internal_type() == "CharField":
+                choices[field.name] = None  
+                continue
+            if not field.choices:
+                choices[field.name] = None
+                continue
+            choices[field.name] = [
+                {
+                    "value": choice[0],
+                    "label": choice[1],
+                }
+                for choice in field.choices
+            ]      
+        logger.debug(choices)
+        return choices
 
     def get_verbose_names(self, obj):
-        return get_verbose_names(self.Meta.model)
+        verbose_names = {}
+        for field in self.Meta.model._meta.get_fields():
+            if field.get_internal_type() in ["ForeignKey", "ManyToManyField"]:
+                verbose_names[field.name] = None
+                continue
+            verbose_names[field.name] = field.verbose_name
+        logger.debug(verbose_names)
+        return verbose_names
 
     def get_blank_fields(self, obj):
-        return get_blank_fields(self.Meta.model)
+        blank_fields = {}
+        for field in self.Meta.model._meta.get_fields():
+            if field.get_internal_type() in ["ForeignKey", "ManyToManyField"]:
+                blank_fields[field.name] = None
+                continue
+            blank_fields[field.name] = field.blank
+        logger.debug(blank_fields)
+        return blank_fields
 
     def get_max_length(self, obj):
-        return get_max_length(self.Meta.model)
-
-    def get_choices(self, obj):
-        return get_choices(self.Meta.model)
-    
-    
-def get_choices(model):
-    choices = {}
-    for field in model._meta.get_fields():
-        if not field.get_internal_type() == "CharField":
-            choices[field.name] = None  
-            continue
-        if not field.choices:
-            choices[field.name] = None
-            continue
-        choices[field.name] = [
-            {
-                "value": choice[0],
-                "label": choice[1],
-            }
-            for choice in field.choices
-        ]      
-    logger.debug(choices)
-    return choices
-
-
-def get_verbose_names(model):
-    verbose_names = {}
-    for field in model._meta.get_fields():
-        if field.get_internal_type() in ["ForeignKey", "ManyToManyField"]:
-            verbose_names[field.name] = None
-            continue
-        verbose_names[field.name] = field.verbose_name
-    logger.debug(verbose_names)
-    return verbose_names
-
-
-def get_blank_fields(model):
-    blank_fields = {}
-    for field in model._meta.get_fields():
-        if field.get_internal_type() in ["ForeignKey", "ManyToManyField"]:
-            blank_fields[field.name] = None
-            continue
-        blank_fields[field.name] = field.blank
-    logger.debug(blank_fields)
-    return blank_fields
-
-
-def get_max_length(model):
-    max_length = {}
-    for field in model._meta.get_fields():
-        if not field.get_internal_type() == "CharField":
-            max_length[field.name] = None
-            continue
-        max_length[field.name] = field.max_length     
-    logger.debug(max_length)
-    return max_length
+        max_length = {}
+        for field in self.Meta.model._meta.get_fields():
+            if not field.get_internal_type() == "CharField":
+                max_length[field.name] = None
+                continue
+            max_length[field.name] = field.max_length     
+        logger.debug(max_length)
+        return max_length
