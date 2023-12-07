55df1bbb7ed74b6eb7814e944002920c7dc0f6be
Daniel Roithmeier <roithmeierda@gmail.com>
2023-12-07 18:43:54 +0100

    reduced recipe form to single component

--- angular/src/app/forms/recipe-create-form/recipe-create-form.component.ts
+++ angular/src/app/forms/recipe-create-form/recipe-create-form.component.ts
@@ -1,19 +1,14 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { RecipeFormMetaComponent } from '../recipe-form-meta/recipe-form-meta.component';
-import { RecipeFormAmountsComponent } from '../recipe-form-amounts/recipe-form-amounts.component';
-import { RecipeFormStepsComponent } from '../recipe-form-steps/recipe-form-steps.component';
 
 @Component({
   selector: 'app-recipe-create-form',
   standalone: true,
-  imports: [
-    CommonModule,
-    RecipeFormMetaComponent,
-    RecipeFormAmountsComponent,
-    RecipeFormStepsComponent,
-  ],
+  imports: [CommonModule],
   templateUrl: './recipe-create-form.component.html',
   styleUrl: './recipe-create-form.component.css',
 })
-export class RecipeCreateFormComponent {}
+export class RecipeCreateFormComponent {
+  recipeForm = new FormGroup({});
+}
