6424b9a561322799b9f66c1c6ed7610373ba53cb
Daniel Roithmeier <roithmeierda@gmail.com>
2023-12-07 17:28:49 +0100

    fix after renaming component

--- angular/src/app/dialogs/create-recipe-dialog/create-recipe-dialog.component.ts
+++ angular/src/app/dialogs/create-recipe-dialog/create-recipe-dialog.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RecipeService } from '../../services/recipe/recipe.service';
-import { RecipeFormComponent } from '../../forms/recipe-form/recipe-form.component';
+import { RecipeCreateFormComponent } from '../../forms/recipe-create-form/recipe-create-form.component';
 
 @Component({
   selector: 'app-create-recipe-dialog',
   standalone: true,
-  imports: [CommonModule, RecipeFormComponent],
+  imports: [CommonModule, RecipeCreateFormComponent],
   templateUrl: './create-recipe-dialog.component.html',
   styleUrl: './create-recipe-dialog.component.css',
 })
