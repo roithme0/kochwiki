f575ff1da0fef7f03f1233ea6243cc5fa66b5396
Daniel Roithmeier <roithmeierda@gmail.com>
2023-12-07 17:27:22 +0100

    renamed

--- angular/src/app/forms/recipe-create-form/recipe-create-form.component.ts
+++ angular/src/app/forms/recipe-create-form/recipe-create-form.component.ts
@@ -5,7 +5,7 @@ import { RecipeFormAmountsComponent } from '../recipe-form-amounts/recipe-form-a
 import { RecipeFormStepsComponent } from '../recipe-form-steps/recipe-form-steps.component';
 
 @Component({
-  selector: 'app-recipe-form',
+  selector: 'app-recipe-create-form',
   standalone: true,
   imports: [
     CommonModule,
@@ -13,7 +13,7 @@ import { RecipeFormStepsComponent } from '../recipe-form-steps/recipe-form-steps
     RecipeFormAmountsComponent,
     RecipeFormStepsComponent,
   ],
-  templateUrl: './recipe-form.component.html',
-  styleUrl: './recipe-form.component.css',
+  templateUrl: './recipe-create-form.component.html',
+  styleUrl: './recipe-create-form.component.css',
 })
-export class RecipeFormComponent {}
+export class RecipeCreateFormComponent {}
