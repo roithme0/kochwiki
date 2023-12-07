7ad272376fed0587e38c89220b25900798f24e8f
Daniel Roithmeier <roithmeierda@gmail.com>
2023-12-07 19:04:15 +0100

    started making the form reactive

--- angular/src/app/forms/recipe-create-form/recipe-create-form.component.ts
+++ angular/src/app/forms/recipe-create-form/recipe-create-form.component.ts
@@ -1,14 +1,38 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, FormArray } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-recipe-create-form',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './recipe-create-form.component.html',
   styleUrl: './recipe-create-form.component.css',
 })
 export class RecipeCreateFormComponent {
-  recipeForm = new FormGroup({});
+  recipeForm = new FormGroup({
+    name: new FormControl(),
+    image: new FormControl(),
+    origin_name: new FormControl(),
+    origin_url: new FormControl(),
+    original: new FormControl(),
+    amounts: new FormArray([
+      new FormGroup({
+        index: new FormControl(),
+        ingredient: new FormControl(),
+        amount: new FormControl(),
+      }),
+    ]),
+    steps: new FormArray([
+      new FormGroup({
+        index: new FormControl(),
+        description: new FormControl(),
+      }),
+    ]),
+  });
+
+  onSubmit(data: any): void {
+    console.debug('submitting create recipe form: ', data);
+  }
 }
