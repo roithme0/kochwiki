52f20d63b05ba5a3e982ee4f797a46e951a7295f
Daniel Roithmeier <roithmeierda@gmail.com>
2023-12-12 16:04:49 +0100

    generated with quarkus extension

---
+++ quarkus/src/test/java/org/acme/IngredientIT.java
@@ -0,0 +1,8 @@
+package org.acme;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class IngredientIT extends IngredientTest {
+    // Execute the same tests but in packaged mode.
+}
