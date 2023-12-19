4842d57b26bf76497747da0c542c70b825fbc28c
Daniel Roithmeier <roithmeierda@gmail.com>
2023-12-19 13:32:48 +0100

    sort recipes

--- angular/src/app/recipes-grid/recipes-grid.component.ts
+++ angular/src/app/recipes-grid/recipes-grid.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  Signal,
+  WritableSignal,
+  computed,
+  signal,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+
 import { RecipesGridControlsComponent } from '../recipes-grid-controls/recipes-grid-controls.component';
 import { RecipesGridElementComponent } from '../recipes-grid-element/recipes-grid-element.component';
-import { Recipe } from '../interfaces/recipe';
-import { RecipeService } from '../services/recipe/recipe.service';
 import { CreateRecipeDialogComponent } from '../dialogs/create-recipe-dialog/create-recipe-dialog.component';
-import { MatDialog } from '@angular/material/dialog';
+
+import { Recipe } from '../interfaces/recipe';
+
+import { RecipeService } from '../services/recipe/recipe.service';
 
 @Component({
   selector: 'app-recipes-grid',
@@ -21,13 +30,22 @@ import { MatDialog } from '@angular/material/dialog';
   styleUrl: './recipes-grid.component.css',
 })
 export class RecipesGridComponent {
-  recipes: Recipe[] = [];
+  // fetch all recipes
+  // render grid-controls component
+  // render recipes in grid
+  recipes: WritableSignal<Recipe[]> = signal([]);
+  displayedRecipes: Signal<Recipe[]> = computed(() => {
+    var displayedRecipes = this.sortRecipes('name', this.recipes());
+    console.debug('displayedRecipes: ', displayedRecipes);
+    return displayedRecipes;
+  });
 
   constructor(
     private recipeService: RecipeService,
     public router: Router,
     private dialog: MatDialog
   ) {
+    // track changes to recipes
     this.recipeService.recipes$.subscribe(() => {
       this.fetchRecipes();
     });
@@ -38,10 +56,11 @@ export class RecipesGridComponent {
   }
 
   fetchRecipes(): void {
+    // fetch all recipes
     this.recipeService.getAllRecipes().subscribe({
       next: (recipes) => {
         console.debug('fetched recipes: ', recipes);
-        this.recipes = recipes;
+        this.recipes.set(recipes);
       },
       error: (err) => {
         console.error('failed to fetch recipes: ', err);
@@ -49,7 +68,20 @@ export class RecipesGridComponent {
     });
   }
 
+  sortRecipes(sortBy: string, recipes: Recipe[]): Recipe[] {
+    // sort recipes by field
+    const sortedRecipes = [...recipes];
+    return sortedRecipes.sort((a, b) => {
+      if (sortBy === 'name') {
+        return a.name.localeCompare(b.name);
+      }
+      console.debug('sortBy invalid: ', sortBy);
+      return 0;
+    });
+  }
+
   openCreateRecipeDialog(): void {
+    // open dialog to create new recipe
     this.dialog.open(CreateRecipeDialogComponent);
   }
 }
