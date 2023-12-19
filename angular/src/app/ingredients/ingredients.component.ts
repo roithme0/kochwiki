d06d3a88e6f0bcaa8e169b7af54298b43c2780d3
Daniel Roithmeier <roithmeierda@gmail.com>
2023-12-19 14:26:52 +0100

    added comments

--- angular/src/app/ingredients/ingredients.component.ts
+++ angular/src/app/ingredients/ingredients.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { IngredientsGridComponent } from '../ingredients-grid/ingredients-grid.component';
+
 import { PageHeaderService } from '../services/page-header/page-header.service';
 
 @Component({
@@ -11,9 +13,12 @@ import { PageHeaderService } from '../services/page-header/page-header.service';
   styleUrl: './ingredients.component.css',
 })
 export class IngredientsComponent {
+  // set header values
+  // render ingredients-grid component
   constructor(private pageHeaderService: PageHeaderService) {}
 
   ngOnInit() {
+    // set header values
     this.pageHeaderService.setHeadline('Zutaten');
     this.pageHeaderService.setBack('');
   }
