5c4edec680215b3a89cedfbad49005c9928f4a12
Daniel Roithmeier <roithmeierda@gmail.com>
2023-12-19 16:22:28 +0100

    added comments

--- angular/src/app/services/ingredient/ingredient.service.ts
+++ angular/src/app/services/ingredient/ingredient.service.ts
@@ -16,27 +16,29 @@ const backendUrl: string = 'http://localhost:8080';
   providedIn: 'root',
 })
 export class IngredientService {
+  // backend communication for ingredients
   private ingredientsSubject = new Subject<void>();
   ingredients$ = this.ingredientsSubject.asObservable();
 
   constructor(private http: HttpClient) {}
 
   notifyIngredientsChanged() {
+    // notify subscribers that ingredients have changed
     this.ingredientsSubject.next();
   }
 
   getAllIngredients(): Observable<Ingredient[]> {
-    console.debug('GET: fetching all ingredients');
+    console.debug('GET: fetching all ingredients ...');
     return this.http.get<Ingredient[]>(backendUrl + '/ingredients');
   }
 
   getIngredientById(id: number): Observable<Ingredient> {
-    console.debug('GET: fetching ingredient by id: ' + id);
+    console.debug('GET: fetching ingredient by id "' + id + '" ...');
     return this.http.get<Ingredient>(backendUrl + '/ingredients/' + id);
   }
 
   putIngredient(ingredient: Ingredient): Observable<Ingredient> {
-    console.debug('PUT: putting ingredient: ', ingredient);
+    console.debug('PUT: putting ingredient "' + ingredient.name + '" ...');
     return this.http.put<Ingredient>(
       backendUrl + '/ingredients/' + ingredient.id,
       ingredient
@@ -44,12 +46,12 @@ export class IngredientService {
   }
 
   postIngredient(ingredient: Ingredient): Observable<Ingredient> {
-    console.debug('POST: posting ingredient: ', ingredient);
+    console.debug('POST: posting ingredient "' + ingredient + '" ...');
     return this.http.post<Ingredient>(backendUrl + '/ingredients', ingredient);
   }
 
   deleteIngredient(id: number): Observable<number> {
-    console.debug('DELETE: deleting ingredient by id: ' + id);
+    console.debug('DELETE: deleting ingredient by id "' + id + '" ...');
     return this.http.delete<number>(backendUrl + '/ingredients/' + id);
   }
 
