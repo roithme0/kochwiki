540bfec3253c3112197c7e240857a1bf52b822d3
Daniel Roithmeier <roithmeierda@gmail.com>
2024-01-16 11:03:36 +0100

    prepared patch method

--- quarkus/src/main/java/org/acme/IngredientResource.java
+++ quarkus/src/main/java/org/acme/IngredientResource.java
@@ -7,6 +7,7 @@ import org.jboss.logging.Logger;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.PATCH;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
@@ -49,6 +50,36 @@ public class IngredientResource {
         entity.setCarbs(ingredient.getCarbs());
         entity.setProtein(ingredient.getProtein());
         entity.setFat(ingredient.getFat());
+        return entity;
+    }
+
+    @PATCH
+    @Path("/{id}")
+    @Transactional
+    public Ingredient patch(@PathParam("id") Long id, Ingredient ingredient) {
+        log.info("PATCH: patching ingredient with id '" + id + "' ...");
+        Ingredient entity = Ingredient.findById(id);
+        if (ingredient.getName() != null) {
+            entity.setName(ingredient.getName());
+        }
+        if (ingredient.getBrand() != null) {
+            entity.setBrand(ingredient.getBrand());
+        }
+        if (ingredient.getUnit() != null) {
+            entity.setUnit(ingredient.getUnit());
+        }
+        if (ingredient.getKcal() != null) {
+            entity.setKcal(ingredient.getKcal());
+        }
+        if (ingredient.getCarbs() != null) {
+            entity.setCarbs(ingredient.getCarbs());
+        }
+        if (ingredient.getProtein() != null) {
+            entity.setProtein(ingredient.getProtein());
+        }
+        if (ingredient.getFat() != null) {
+            entity.setFat(ingredient.getFat());
+        }
         return entity;
     }
 
