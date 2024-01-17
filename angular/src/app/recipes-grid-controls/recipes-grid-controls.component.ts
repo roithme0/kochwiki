137100a0bc0eb61d37f3f87d8de8450a916c559b
Daniel Roithmeier <roithmeierda@gmail.com>
2024-01-17 12:54:55 +0100

    added dummy search bar

--- angular/src/app/recipes-grid-controls/recipes-grid-controls.component.ts
+++ angular/src/app/recipes-grid-controls/recipes-grid-controls.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, FormControl, ReactiveFormsModule } from '@angular/forms';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-recipes-grid-controls',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatAutocompleteModule,
+    MatIconModule,
+  ],
   templateUrl: './recipes-grid-controls.component.html',
-  styleUrl: './recipes-grid-controls.component.css'
+  styleUrl: './recipes-grid-controls.component.css',
 })
 export class RecipesGridControlsComponent {
-
+  searchControl: FormControl = new FormControl('');
 }
