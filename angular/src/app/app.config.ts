f342067feee3af342790534bc7519137d1032804
Daniel Roithmeier <roithmeierda@gmail.com>
2024-01-26 18:18:11 +0100

    reinstalled material ui for custom styles

--- angular/src/app/app.config.ts
+++ angular/src/app/app.config.ts
@@ -11,7 +11,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideClientHydration(),
     provideAnimations(),
-    provideAnimations(),
     provideHttpClient(withFetch()),
   ],
 };
