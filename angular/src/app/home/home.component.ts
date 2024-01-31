723ce69c316008fafec023dc557948fc42dcd890
Daniel Roithmeier <roithmeierda@gmail.com>
2024-01-31 15:21:30 +0100

    make links mat buttons

--- angular/src/app/home/home.component.ts
+++ angular/src/app/home/home.component.ts
@@ -2,12 +2,14 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet, RouterLink } from '@angular/router';
 
+import { MatButtonModule } from '@angular/material/button';
+
 import { PageHeaderService } from '../shared/services/page-header.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink],
+  imports: [CommonModule, RouterOutlet, RouterLink, MatButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
