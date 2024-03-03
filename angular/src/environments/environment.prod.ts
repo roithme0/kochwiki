7f69484854b7b96774dc4f7402d950bf6b7a7026
Daniel Roithmeier <roithmeierda@gmail.com>
2024-03-03 16:41:30 +0100

    use docker network and only one dockerfile per app

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  backendUrl: 'http://192.168.178.155:8888',
+  backendUrl: 'http://quarkus-app:8080',
   name: 'prod',
 };
--- angular/src/environments/environment.stag.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  backendUrl: 'http://192.168.178.155:8889',
-  name: 'stag',
-};
