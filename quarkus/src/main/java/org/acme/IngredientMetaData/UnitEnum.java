c34d3cce2a45e428a2527714ba7d37a616e95200
Daniel Roithmeier <daniel.roithmeier@feynsinn.de>
2024-03-05 13:25:52 +0100

    made param final

--- quarkus/src/main/java/org/acme/IngredientMetaData/UnitEnum.java
+++ quarkus/src/main/java/org/acme/IngredientMetaData/UnitEnum.java
@@ -23,7 +23,7 @@ public enum UnitEnum {
      * Constructor.
      * @param newUnit unit to set.
      */
-    private UnitEnum(String newUnit) {
+    private UnitEnum(final String newUnit) {
         unit = newUnit;
     }
 
